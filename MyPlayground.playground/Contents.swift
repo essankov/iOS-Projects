512941ea44966777ca36947784a67b81edb9633c
Issa Alkhamees <issaalkhamees@Issas-MacBook-Pro.local>
2018-09-13 15:48:41 -0500

    playground :)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var health : Int = 100
+let maxHealth : Int = 150
+
+let name : String = "Essa"
+var c = name.count
+var fC = name.first
+var lC = name.last
+
+var age : Int = 26
+var Age : String = "I'm \(age) years old."
+
+// index
+let info : (String, Int) = ("Essa", 26)
+let infoName : String = info.0
+let infoAge : Int = info.1
+
+//array
+var array :[String] = ["Essa", "loooool", "what's up"]
+
+
+//range
+
+let maxNum = 1...5
+for num in maxNum
+{
+    print(num)
+}
